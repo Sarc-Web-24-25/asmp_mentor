f8f1f5911df6986f599f3436d06fa424e44957dc
PranitaR12 <96475629+PranitaR12@users.noreply.github.com>
2023-06-25 23:13:02 +0530

    Update settings.py
    password hataya hehe

--- asmp_mentor/asmp_mentor/asmp_mentor/settings.py
+++ asmp_mentor/asmp_mentor/asmp_mentor/settings.py
@@ -114,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # EMAIL_HOST = 'smtp.gmail.com'
 # EMAIL_PORT = 587
 # EMAIL_HOST_USER = 'prandive12@gmail.com'  
-# EMAIL_HOST_PASSWORD = 'xirtmqmaueferxkf'  
+# EMAIL_HOST_PASSWORD = ''  
 # EMAIL_USE_TLS = True
 # DEFAULT_FROM_EMAIL = 'prandive12@gmail.com' 
 
