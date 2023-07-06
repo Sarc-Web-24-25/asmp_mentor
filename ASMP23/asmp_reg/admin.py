d6503be639c1d3ea60e178e20fbdb1ace1de00f7
akash-banger2 <akashbanger@troopapp.in>
2023-07-06 11:41:38 +0530

    done

--- ASMP23/asmp_reg/admin.py
+++ ASMP23/asmp_reg/admin.py
@@ -3,20 +3,18 @@ from django.contrib import admin
 # Register your models here.
 from .models import Registration
 
-# admin.site.register(Registration)
+admin.site.register(Registration)
 
-class RegistrationAdmin(admin.ModelAdmin):
-    list_display = ['id', 'token', 'fullname', 'department', 'department_other', 'degree', 'degree_other', 'hostel', 'year', 'other_year', 'contact', 'email', 'linkedin', 'city', 'country', 'dept_mentees', 'designation', 'company_name', 'work_profile', 'pref', 'field_other', 'mentees', 'preference_no_preference', 'preference_final_year_undergrad', 'preference_other_undergrad', 'preference_mtech_students', 'preference_phd_students', 'buddy', 'isNew']
-
-
+# class RegistrationAdmin(admin.ModelAdmin):
+#     list_display = ['token', 'fullname', 'isNew']
 
 
-class NewRegistrationsAdmin(admin.ModelAdmin):
-    list_display = ['id', 'fullname', 'isNew']
+class NewRegistrations(admin.ModelAdmin):
+    list_display = ['token', 'fullname', 'isNew']
 
     def get_queryset(self, request):
         queryset = super().get_queryset(request)
         return queryset.filter(isNew=True)
     
-admin.site.register(Registration, RegistrationAdmin)
+admin.site.register(Registration, NewRegistrations)
 
